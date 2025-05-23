ac2a70c6707229c32b23cbe2ef070e54c3f0565f
Christian Kiernan <184921820+ChristianKiernan@users.noreply.github.com>
2025-05-23 08:14:48 -0400

    Reconfigure project to exclude views

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 require('dotenv').config(); 
 const express = require('express');
 const expressSession = require('express-session');
-const expressLayouts = require('express-ejs-layouts');
 const bodyParser = require('body-parser')
 const passport = require('passport');
 const app = express();
-const path = require('node:path');
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.use(bodyParser.json())
 
 const { PrismaSessionStore } = require('@quixo3/prisma-session-store');
 const { PrismaClient } = require('@prisma/client');
@@ -31,8 +28,12 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.urlencoded({ extended: true }));
-app.use(expressLayouts);
 
 console.log("Nodemon is working");
-app.get("/", (req, res) => res.send('Hello, World'));
-app.listen(3000, () => console.log('App listening on localhost:3000'));
+app.get("/", (req, res) => {
+    res.json({
+        message: 'Welcome to the App',
+    });
+});
+
+app.listen(5000, () => console.log('App listening on localhost:5000'));
