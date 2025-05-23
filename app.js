344f72565a72dbb508d40a505faaa5fe1653c56c
Christian Kiernan <184921820+ChristianKiernan@users.noreply.github.com>
2025-05-23 07:13:30 -0400

    Extend boilerplate

--- app.js
+++ app.js
@@ -1,35 +1,37 @@
-require('dotenv').config(); //Needs to come before local imports
+require('dotenv').config(); 
 const express = require('express');
 const expressSession = require('express-session');
+const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser')
 const passport = require('passport');
 const app = express();
+const path = require('node:path');
 
-//If using MVC setup
-// const path = require('node:path');
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-//If using Prisma ORM for database (if not, delete dev dependencies)
-// const { PrismaSessionStore } = require('@quixo3/prisma-session-store');
-// const { PrismaClient } = require('@prisma/client');
-// app.use(
-// 	expressSession({
-// 		cookie: {
-// 			maxAge: 7 * 24 * 60 * 60 * 1000,
-// 		},
-// 		secret: 'a santa at nasa',
-// 		resave: true,
-// 		saveUninitialized: true,
-// 		store: new PrismaSessionStore(new PrismaClient(), {
-// 			checkPeriod: 2 * 60 * 1000,
-// 			dbRecordIdIsSessionId: true,
-// 			dbRecordIdFunction: undefined,
-// 		}),
-// 	})
-// );
+const { PrismaSessionStore } = require('@quixo3/prisma-session-store');
+const { PrismaClient } = require('@prisma/client');
+app.use(
+	expressSession({
+		cookie: {
+			maxAge: 10 * 60 * 1000, // 10 minutes
+		},
+		secret: 'a santa at nasa',
+		resave: true,
+		saveUninitialized: true,
+		store: new PrismaSessionStore(new PrismaClient(), {
+			checkPeriod: 2 * 60 * 1000,
+			dbRecordIdIsSessionId: true,
+			dbRecordIdFunction: undefined,
+		}),
+	})
+);
 
-// app.use(passport.initialize());
-// app.use(passport.session());
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.urlencoded({ extended: true }));
+app.use(expressLayouts);
 
 console.log("Nodemon is working");
 app.get("/", (req, res) => res.send('Hello, World'));
