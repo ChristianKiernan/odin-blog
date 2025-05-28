22a0b9b5d2665930b5eb8f053958d0023b143b7a
Christian Kiernan <184921820+ChristianKiernan@users.noreply.github.com>
2025-05-28 07:04:37 -0400

    Add middleware to enable access to req.user

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 require('dotenv').config(); 
 const express = require('express');
 const expressSession = require('express-session');
-const bodyParser = require('body-parser')
-const passport = require('passport');
+// const bodyParser = require('body-parser')
+const passport = require('./config/passport');
+const jwt = require('jsonwebtoken');
+const authRouter = require('./routes/authRouter')
 const userRouter = require('./routes/userRouter');
 const postRouter = require('./routes/postRouter')
 const app = express();
 
-app.use(bodyParser.json())
+// app.use(bodyParser.json())
 
 const { PrismaSessionStore } = require('@quixo3/prisma-session-store');
 const { PrismaClient } = require('@prisma/client');
@@ -29,14 +31,21 @@ app.use(
 
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
-console.log("Nodemon is working");
 app.get("/", (req, res) => {
     res.json({
         message: 'Welcome to the App',
     });
 });
+
+app.use((req, res, next) => {
+	res.locals.currentUser = req.user;
+	next();
+});
+
+app.use('/auth', authRouter);
 app.use('/users', userRouter);
 app.use('/posts', postRouter);
 app.listen(5000, () => console.log('App listening on localhost:5000'));
