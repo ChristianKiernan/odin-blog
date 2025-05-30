cdb985e1c858d27c4267da39c953e879baca6c55
Christian Kiernan <184921820+ChristianKiernan@users.noreply.github.com>
2025-05-30 05:37:20 -0400

    Add functionality to delete a post

--- controllers/post.js
+++ controllers/post.js
@@ -72,5 +72,26 @@ exports.updatePostById = async (req, res, next) => {
 	}
 };
 
-exports.deleteById;
-//TODO
+exports.deletePostById = async (req, res, next) => {
+	try {
+		const { id } = req.params;
+
+		const existingPost = await prisma.post.findUnique({
+			where: { id: parseInt(id) },
+		});
+
+		if (!existingPost) {
+			return next(new AppError('Post not found.', 404));
+		}
+
+		if (existingPost.authorId !== req.user.id && !req.user.isAdmin) {
+			return next(new AppError('Unauthorized to delete this post.', 403));
+		}
+
+		await prisma.post.delete({ where: { id: parseInt(id) } });
+
+		res.status(200).json({ message: 'Post deleted successfully.' });
+	} catch (err) {
+		next(err);
+	}
+};
