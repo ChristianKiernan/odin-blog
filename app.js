a1a1ccead82be2849e59c1aaa7810405fbdcb525
Christian Kiernan <184921820+ChristianKiernan@users.noreply.github.com>
2025-05-30 01:56:40 -0400

    Reorganize code layout

--- app.js
+++ app.js
@@ -1,24 +1,31 @@
-require('dotenv').config(); 
+require('dotenv').config();
 const express = require('express');
 const passport = require('./config/passport');
 const jwt = require('jsonwebtoken');
 const errorHandler = require('./middleware/errorHandler');
-const authRouter = require('./routes/authRouter')
+
+const authRouter = require('./routes/authRouter');
 const userRouter = require('./routes/userRouter');
-const postRouter = require('./routes/postRouter')
+const postRouter = require('./routes/postRouter');
+
 const app = express();
-const { PrismaClient } = require('@prisma/client');
 
+// Mddlewares
 app.use(passport.initialize());
-app.use(express.json())
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(errorHandler);
 
-app.use((req, res, next) => {
-	next();
-});
-
+// Routes
 app.use('/auth', authRouter);
 app.use('/users', userRouter);
 app.use('/posts', postRouter);
-app.listen(5000, () => console.log('App listening on localhost:5000'));
+
+//Error handler
+app.use(errorHandler);
+
+// 404 handler
+app.use((req, res, next) => {
+  res.status(404).json({ message: 'Route not found' });
+});
+
+app.listen(5000, () => console.log(`App listening on http://localhost:5000`));
