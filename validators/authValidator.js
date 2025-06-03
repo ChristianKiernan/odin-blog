ed507a5396bf213d17d3071cd26b6860b19eeb34
Christian Kiernan <184921820+ChristianKiernan@users.noreply.github.com>
2025-06-03 07:31:35 -0400

    Add validation for user login

---
+++ validators/authValidator.js
@@ -0,0 +1,20 @@
+const { body, validationResult } = require('express-validator');
+const AppError = require('../utils/appError');
+
+exports.validateLogin = [
+	body('username').notEmpty().withMessage('Username is required'),
+	body('password').notEmpty().withMessage('Password is required'),
+
+	(req, res, next) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+            //Convert express-validator errors to custom errors
+			const message = errors
+				.array()
+				.map((err) => `${err.param}: ${err.msg}`)
+				.join('; ');
+			return next(new AppError(message, 400));
+		}
+		next();
+	},
+];
