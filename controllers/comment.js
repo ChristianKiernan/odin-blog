412bc76136ae891527fea4b5e8bff5142f9753c0
Christian Kiernan <184921820+ChristianKiernan@users.noreply.github.com>
2025-06-26 18:31:42 -0400

    Bug fixes: Modify comment endpoints

--- controllers/comment.js
+++ controllers/comment.js
@@ -10,7 +10,15 @@ exports.getComments = async (req, res, next) => {
 		}
 
 		const comments = await prisma.comment.findMany({
-			where: { postId: postId },
+			where: { postId },
+			include: {
+				author: {
+					select: {
+						id: true,
+						username: true,
+					},
+				},
+			},
 		});
 
 		if (!comments.length) {
@@ -39,9 +47,14 @@ exports.createComment = async (req, res, next) => {
 
 		const comment = await prisma.comment.create({
 			data: {
-				content: content,
+				content,
 				authorId: req.user.id,
-				postId: postId,
+				postId,
+			},
+			include: {
+				author: {
+					select: { id: true, username: true },
+				},
 			},
 		});
 
@@ -53,34 +66,45 @@ exports.createComment = async (req, res, next) => {
 
 exports.editById = async (req, res, next) => {
 	try {
-		const id = Number(req.params.commentId);
-
-		if (isNaN(id)) {
+		// 1) Parse and validate the incoming commentId param
+		const commentId = Number(req.params.commentId);
+		if (isNaN(commentId)) {
 			return next(new AppError(`Invalid comment ID ${commentId}.`, 400));
 		}
 
 		const { content } = req.body;
+		if (!content) {
+			return next(new AppError('Content is required.', 400));
+		}
 
+		// 2) Fetch existing comment
 		const existingComment = await prisma.comment.findUnique({
-			where: { id: id },
+			where: { id: commentId },
 		});
-
 		if (!existingComment) {
 			return next(new AppError('Comment not found.', 404));
 		}
 
+		// 3) Authorization: only the author may edit
 		if (existingComment.authorId !== req.user.id) {
 			return next(
 				new AppError('Unauthorized to update this comment.', 403)
 			);
 		}
 
+		// 4) Perform update and include author info
 		const updatedComment = await prisma.comment.update({
-			where: { id: id },
+			where: { id: commentId },
 			data: { content },
+			include: {
+				author: {
+					select: { id: true, username: true },
+				},
+			},
 		});
 
-		res.status(200).json({ data: { updatedComment } });
+		// 5) Respond with the updated comment
+		return res.status(200).json({ data: { updatedComment } });
 	} catch (err) {
 		next(err);
 	}
@@ -88,30 +112,35 @@ exports.editById = async (req, res, next) => {
 
 exports.deleteById = async (req, res, next) => {
 	try {
-		const id = Number(req.params.commentId);
-
-		if (isNaN(id)) {
-			return next(new AppError('Invalid comment Id.', 400));
+		// 1) Parse and validate the incoming param
+		const commentId = Number(req.params.commentId);
+		if (isNaN(commentId)) {
+			return next(new AppError(`Invalid comment ID ${commentId}.`, 400));
 		}
 
+		// 2) Fetch the comment
 		const existingComment = await prisma.comment.findUnique({
-			where: { id: id },
+			where: { id: commentId },
 		});
-
 		if (!existingComment) {
 			return next(new AppError('Comment not found.', 404));
 		}
 
+		// 3) Authorization check
 		if (existingComment.authorId !== req.user.id && !req.user.isAdmin) {
 			return next(
 				new AppError('Unauthorized to delete this comment.', 403)
 			);
 		}
 
-		await prisma.comment.delete({ where: { id: id } });
+		// 4) Delete it
+		await prisma.comment.delete({
+			where: { id: commentId },
+		});
 
-		res.status(204).send();
+		// 5) Send No Content
+		return res.status(204).send();
 	} catch (err) {
-		next(err);
+		return next(err);
 	}
 };
